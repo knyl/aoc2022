7817de2c88733c5af43ccf8e9226ce37e3ede171
Kristina Nylander <kristina.nylander@kvirkla.se>
2022-12-03 08:25:17 +0100

    Solutions day 3, scala

---
+++ scala/src/main/scala/Day03/Day03.scala
@@ -0,0 +1,38 @@
+package Day03
+
+import scala.annotation.tailrec
+import scala.io.Source
+
+def findDuplicate(line: String): Char =
+  val (rucksack1, rucksack2) = line.splitAt(line.length/2)
+  rucksack1.toSet.intersect(rucksack2.toSet).head
+
+def getScore(char: Char): Int =
+  if char.isLower then
+    char - 'a' + 1
+  else
+    char - 'A' + 27
+
+def findGroupBadge(rucksacks: List[String]): Char =
+  rucksacks
+    .map(line => line.toSet)
+    .reduce((items1, items2) => items1.intersect(items2))
+    .head
+
+@main
+def main(): Unit =
+  val lines = Source.fromResource("day03.txt").getLines().toList
+
+  val pt1 = lines
+    .map(findDuplicate)
+    .map(getScore)
+    .sum
+
+  val pt2 = lines
+      .sliding(3, 3)
+      .map(findGroupBadge)
+      .map(getScore)
+      .sum
+
+  println("Pt1: " + pt1)
+  println("Pt2: " + pt2)
