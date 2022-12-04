5757eb0a41b451d0b97e91d3b69f44993bc68270
Kristina Nylander <kristina.nylander@kvirkla.se>
2022-12-04 08:01:03 +0100

    Golf day 4 scala

--- scala/src/main/scala/day04/Day04.scala
+++ scala/src/main/scala/day04/Day04.scala
@@ -6,17 +6,11 @@ case class Assignment(from: Int, to: Int):
   val range: Set[Int] = Set.from(from to to)
 
   def fullyContains(other: Assignment): Boolean =
-    range.contains(other.to) && range.contains(other.from)
+    other.range.subsetOf(range)
 
   def anyOverlap(other: Assignment): Boolean =
-    range.contains(other.to) || range.contains(other.from)
+    range.intersect(other.range).nonEmpty
 
-
-def fullyContains(p1: Assignment, p2: Assignment): Boolean =
-  p1.fullyContains(p2) || p2.fullyContains(p1)
-
-def anyOverlap(p1: Assignment, p2: Assignment): Boolean =
-  p1.anyOverlap(p2) || p2.anyOverlap(p1)
 
 def parseInput(line: String): (Assignment, Assignment) = line match {
   case s"$p1-$p2,$p3-$p4" => (Assignment(p1.toInt, p2.toInt), Assignment(p3.toInt, p4.toInt))
@@ -28,8 +22,8 @@ def main(): Unit =
   val input = Source.fromResource("day04.txt").getLines().toList
   val assignments = input.map(parseInput)
 
-  val pt1 = assignments.count(fullyContains)
-  val pt2 = assignments.count(anyOverlap)
+  val pt1 = assignments.count((p1, p2) => p1.fullyContains(p2) || p2.fullyContains(p1))
+  val pt2 = assignments.count((p1, p2) => p1.anyOverlap(p2))
 
   println("Pt1: " + pt1)
   println("Pt2: " + pt2)
