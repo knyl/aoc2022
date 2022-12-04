3bfde639ce67c196ce7da0b62992009965d356ff
Kristina Nylander <kristina.nylander@kvirkla.se>
2022-12-04 21:20:35 +0100

    Solutions day 3 rust

--- rust/src/day03.rs
+++ rust/src/day03.rs
@@ -1,32 +1,48 @@
-use std::collections::hash_map::RandomState;
-use std::collections::HashSet;
-//use itertools::Itertools;
+use im::HashSet;
+use itertools::Itertools;
 
 fn pt1(input: &str) -> u32 {
     input.lines()
-        .map(handle_line)
+        .map(|line| line.split_at(line.len() / 2))
+        .map(|(l1, l2)| (create_hash_set(l1), create_hash_set(l2)))
+        .map(|(set1, set2)|
+            set1.intersection(set2).iter().next().unwrap().clone())
+        .map(score)
         .sum()
 }
 
-fn handle_line(line: &str) -> u32 {
-    let (l1, l2) = line.split_at(line.len() / 2);
-    let set1: HashSet<char, RandomState> = HashSet::from_iter(l1.chars());
-    let set2: HashSet<char, RandomState> = HashSet::from_iter(l2.chars());
-    let char = set1.intersection(&set2).next().unwrap();
-    score(char)
+fn pt2(input: &str) -> u32 {
+    input
+        .lines()
+        .map(create_hash_set)
+        .chunks(3)
+        .into_iter()
+        .map(|chunk| {
+            chunk
+                .reduce(|set1, set2| set1.intersection(set2))
+                .unwrap()
+                .iter().next().unwrap().clone()
+        })
+        .map(score)
+        .sum()
 }
 
-fn score(char: &char) -> u32 {
+fn create_hash_set(l1: &str) -> HashSet<char> {
+    l1.chars().collect::<HashSet<char>>()
+}
+
+fn score(char: char) -> u32 {
     if char.is_ascii_lowercase() {
-        *char as u32 - 'a' as u32 + 1
+        char as u32 - 'a' as u32 + 1
     } else {
-        *char as u32 - 'A' as u32 + 27
+        char as u32 - 'A' as u32 + 27
     }
 }
 
 pub fn day03() {
     let input = include_str!("../input/day03.txt");
     println!("Part 1: {}", pt1(input));
+    println!("Part 2: {}", pt2(input));
 }
 
 
@@ -45,5 +61,10 @@ CrZsJsPPZsGzwwsLwLmpwMDw";
     #[test]
     fn test_input_pt1() {
         assert_eq!(pt1(INPUT.trim()), 157)
+    }
+
+    #[test]
+    fn test_input_pt2() {
+        assert_eq!(pt2(INPUT.trim()), 70)
     }
 }
